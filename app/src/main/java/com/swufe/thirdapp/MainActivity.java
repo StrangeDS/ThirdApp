20f6aa623272453ee301707062c35c73390a110d
StrangeDS <1023221456@qq.com>
2020-09-21 21:01:37 +0800

    两队计分器

--- app/src/main/java/com/swufe/thirdapp/MainActivity.java
+++ app/src/main/java/com/swufe/thirdapp/MainActivity.java
@@ -14,43 +14,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView score = findViewById(R.id.score);
     }
 
-    public  void btn1(View v){
-        TextView score = findViewById(R.id.score);
-        String res = score.getText().toString().split(":")[0];
-        int score_new = Integer.parseInt(score.getText().toString().split(":")[1]) + 1;
-        score.setText(res +":"+ score_new);
+    public  void btn1add1(View v){
+        TextView score = findViewById(R.id.score1);
+        String res = score.getText().toString();
+        int score_new = Integer.parseInt(res) + 1;
+        score.setText("" + score_new);
     }
 
-    public  void btn2(View v){
-        TextView score = findViewById(R.id.score);
-        String res = score.getText().toString().split(":")[0];
-        int score_new = Integer.parseInt(score.getText().toString().split(":")[1]) + 2;
-        score.setText(res +":"+ score_new);
+    public  void btn1add2(View v){
+        TextView score = findViewById(R.id.score1);
+        String res = score.getText().toString();
+        int score_new = Integer.parseInt(res) + 2;
+        score.setText("" + score_new);
     }
 
-    public  void btn3(View v){
-        TextView score = findViewById(R.id.score);
-        String res = score.getText().toString().split(":")[0];
-        int score_new = Integer.parseInt(score.getText().toString().split(":")[1]) + 3;
-        score.setText(res +":"+ score_new);
+    public  void btn1add3(View v){
+        TextView score = findViewById(R.id.score1);
+        String res = score.getText().toString();
+        int score_new = Integer.parseInt(res) + 3;
+        score.setText("" + score_new);
+    }
+
+    public  void btn2add1(View v){
+        TextView score = findViewById(R.id.score2);
+        String res = score.getText().toString();
+        int score_new = Integer.parseInt(res) + 1;
+        score.setText("" + score_new);
+    }
+
+    public  void btn2add2(View v){
+        TextView score = findViewById(R.id.score2);
+        String res = score.getText().toString();
+        int score_new = Integer.parseInt(res) + 2;
+        score.setText("" + score_new);
+    }
+
+    public  void btn2add3(View v){
+        TextView score = findViewById(R.id.score2);
+        String res = score.getText().toString();
+        int score_new = Integer.parseInt(res) + 3;
+        score.setText("" + score_new);
     }
 
     public  void btn0(View v){
-        TextView score = findViewById(R.id.score);
-        String res = score.getText().toString().split(":")[0];
-        score.setText(res +":0");
-    }
-
-    public  void btnr1(View v){
-        TextView score = findViewById(R.id.score);
-        String res = score.getText().toString().split(":")[0];
-        int score_new = Integer.parseInt(score.getText().toString().split(":")[1]) - 1;
-        if(score_new < 0){
-            score_new = 0;
-        }
-        score.setText(res +":"+ score_new);
+        TextView score1 = findViewById(R.id.score1);
+        TextView score2 = findViewById(R.id.score2);
+        score1.setText("0");
+        score2.setText("0");
     }
 }
