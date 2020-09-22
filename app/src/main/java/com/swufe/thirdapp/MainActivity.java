ca4b3bd7e6ab25f0a8f43a09eb148d66eb56f0a5
StrangeDS <1023221456@qq.com>
2020-09-22 21:48:16 +0800

    可配置汇率的汇率转换器

--- app/src/main/java/com/swufe/thirdapp/MainActivity.java
+++ app/src/main/java/com/swufe/thirdapp/MainActivity.java
@@ -1,67 +1,69 @@
 package com.swufe.thirdapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    float dollar = (float) 0.147894;
+    float pound = (float) 0.114358;
+    float euro = (float) 0.124778;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public  void btn1(View v){
+    public  void btn(View v){
         TextView input = findViewById(R.id.inp);
         TextView output = findViewById(R.id.outp);
-        Button b = findViewById(R.id.btn1);
-        String str = input.getText().toString();
-        if(str.isEmpty()){
-            output.setText("0");
-        }
-        else{
-            float result = Float.parseFloat(str);
-            float rate = Float.parseFloat(b.getText().toString().split(":")[1]);
-            result = result * rate;
-            output.setText("" + result);
+        if(input.getText().toString().isEmpty()) {
+            //no input
+            Toast.makeText(this, "请输入人民币金额", Toast.LENGTH_SHORT).show();
+        }else{
+            String str = input.getText().toString();
+            Float result;
+            if(v.getId() == R.id.btn1){
+                result = Float.parseFloat(str) * dollar;
+                output.setText("" + result);
+            }else if(v.getId() == R.id.btn2){
+                result = Float.parseFloat(str) * pound;
+                output.setText("" + result);
+            }else if(v.getId() == R.id.btn3){
+                result = Float.parseFloat(str) * euro;
+                output.setText("" + result);
+            }
         }
     }
 
-    public  void btn2(View v){
-        TextView input = findViewById(R.id.inp);
-        TextView output = findViewById(R.id.outp);
-        Button b = findViewById(R.id.btn2);
-        String str = input.getText().toString();
-        if(str.isEmpty()){
-            output.setText("0");
-        }
-        else{
-            float result = Float.parseFloat(str);
-            float rate = Float.parseFloat(b.getText().toString().split(":")[1]);
-            result = result * rate;
-            output.setText("" + result);
-        }
+    public  void btn_conifg(View v){
+        Intent config = new Intent(this, MainActivity2.class);
+        config.putExtra("dollar_rate", dollar);
+        config.putExtra("pound_rate", pound);
+        config.putExtra("euro_rate", euro);
+        startActivityForResult(config,1);
     }
 
-    public  void btn3(View v){
-        TextView input = findViewById(R.id.inp);
-        TextView output = findViewById(R.id.outp);
-        Button b = findViewById(R.id.btn3);
-        String str = input.getText().toString();
-        if(str.isEmpty()){
-            output.setText("0");
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1 && resultCode==2){
+            Bundle rate = data.getExtras();
+            Toast.makeText(this, "配置更新中", Toast.LENGTH_SHORT).show();
+            dollar = rate.getFloat("dollar_rate", 0.0f);
+            pound = rate.getFloat("pound_rate", 0.0f);
+            euro = rate.getFloat("euro_rate", 0.0f);
+            Log.i("TAG","dollar_rate=" + dollar);
+            Toast.makeText(this, "配置已更新", Toast.LENGTH_SHORT).show();
         }
-        else{
-            float result = Float.parseFloat(str);
-            float rate = Float.parseFloat(b.getText().toString().split(":")[1]);
-            result = result * rate;
-            output.setText("" + result);
-        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/swufe/thirdapp/MainActivity2.java
@@ -0,0 +1,49 @@
+package com.swufe.thirdapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.sql.ResultSet;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    float dollar;
+    float pound;
+    float euro;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        dollar = intent.getFloatExtra("dollar_rate", 0.0f);
+        pound = intent.getFloatExtra("pound_rate", 0.0f);
+        euro = intent.getFloatExtra("euro_rate", 0.0f);
+        EditText ett1 = findViewById(R.id.ett1);
+        EditText ett2 = findViewById(R.id.ett2);
+        EditText ett3 = findViewById(R.id.ett3);
+        ett1.setText("" + dollar);
+        ett2.setText("" + pound);
+        ett3.setText("" + euro);
+    }
+
+    public void btn_save(View v){
+        EditText ett1 = findViewById(R.id.ett1);
+        dollar = Float.parseFloat(ett1.getText().toString());
+        EditText ett2 = findViewById(R.id.ett2);
+        pound = Float.parseFloat(ett2.getText().toString());
+        EditText ett3 = findViewById(R.id.ett3);
+        euro = Float.parseFloat(ett3.getText().toString());
+        Intent config = getIntent();
+        Bundle rate = new Bundle();
+        rate.putFloat("dollar_rate", dollar);
+        rate.putFloat("pound_rate", pound);
+        rate.putFloat("euro_rate", euro);
+        config.putExtras(rate);
+        setResult(2,config);
+        finish();
+    }
+}
